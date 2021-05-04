90a626771ccfcd0709225e62b78d9748a166b5bb
nafil-gigsboard <nafil@gigsboard.com>
2021-05-05 01:03:20 +0530

    bugfix try 8

--- app/Library/CovinApi/CovinLibrary.php
+++ app/Library/CovinApi/CovinLibrary.php
@@ -12,27 +12,28 @@ class CovinLibrary
     const API_URL = 'https://cdn-api.co-vin.in/api/v2/';
 
 	public static function getState() {
-        // $client = new \GuzzleHttp\Client();
-        // $request = $client->get(self::API_URL . 'https://cdn-api.co-vin.in/api/v2/admin/location/states');
-        // $response = $request->getBody();
-        // $states = json_decode($response)->states;
-        // return $states;
-
-        $url = 'https://cdn-api.co-vin.in/api/v2/admin/location/states';
-        $crl = curl_init();
-        
-        curl_setopt($crl, CURLOPT_URL, $url);
-        curl_setopt($crl, CURLOPT_FRESH_CONNECT, true);
-        curl_setopt($crl, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($crl);
-        
-        if(!$response){
-            return [];
-        }
-        return $response;
+        return [];
+        $client = new \GuzzleHttp\Client();
+        $request = $client->get(self::API_URL . 'https://cdn-api.co-vin.in/api/v2/admin/location/states');
+        $response = $request->getBody();
         $states = json_decode($response)->states;
         return $states;
-        curl_close($crl);
+
+        // $url = 'https://cdn-api.co-vin.in/api/v2/admin/location/states';
+        // $crl = curl_init();
+        
+        // curl_setopt($crl, CURLOPT_URL, $url);
+        // curl_setopt($crl, CURLOPT_FRESH_CONNECT, true);
+        // curl_setopt($crl, CURLOPT_RETURNTRANSFER, true);
+        // $response = curl_exec($crl);
+        
+        // if(!$response){
+        //     return [];
+        // }
+        // return $response;
+        // $states = json_decode($response)->states;
+        // return $states;
+        // curl_close($crl);
     }
 
     public static function getDistricts($state_id) {
