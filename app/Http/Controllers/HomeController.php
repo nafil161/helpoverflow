0190cb4dc8cfecdcd021cff0e60c2bb7c5517c1c
nafil-gigsboard <nafil@gigsboard.com>
2021-05-05 00:08:41 +0530

    testing

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function home(){
-        $states = CovinLibrary::getState();
+        return $states = CovinLibrary::getState();
         return view('home', compact('states'));
     }
 
