36bacee280096fa03f35aa1f7ab5ecd3a8d0d034
nafil-gigsboard <nafil@gigsboard.com>
2021-05-05 00:55:15 +0530

    curl bugfix try5

--- app/Library/CovinApi/CovinLibrary.php
+++ app/Library/CovinApi/CovinLibrary.php
@@ -13,8 +13,7 @@ class CovinLibrary
 
 	public static function getState() {
         $client = new \GuzzleHttp\Client();
-        $request = $client->get(self::API_URL . 'admin/location/states' , ['headers' => ['user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36',
-        'accept'=>'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9']]);
+        $request = $client->get('https://cdn-api.co-vin.in/api/v2/admin/location/states');
         $response = $request->getBody();
         $states = json_decode($response)->states;
         return $states;
