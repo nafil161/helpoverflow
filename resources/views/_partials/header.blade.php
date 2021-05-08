577d6c4023e0254982c9ebb739101d953143b15b
nafil161 <nafil161@gmail.com>
2021-05-08 14:06:16 +0530

    logo updated

--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -1,6 +1,6 @@
 <!-- Preloader -->
 <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="{{asset('assets/dist/img/AdminLTELogo.png')}}" alt="HelpOverflow" height="60" width="60">
+    <img class="animation__shake" src="{{asset('assets/dist/img/logo.png')}}" alt="HelpOverflow" height="75" width="150">
 </div>
 
 <!-- Navbar -->
@@ -27,7 +27,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="{{asset('assets/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{asset('assets/dist/img/shortLogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light"><b>HelpOverflow</b></span>
     </a>
 
